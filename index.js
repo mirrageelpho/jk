b615772c4ea74f43d8f9afca00a9d328dc1b7b9b
aeciocleber <aecio.pires@clinicadaobesidade.com.br>
2020-06-25 09:27:18 -0300

    teste stage

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.send('jenkins depoly! 2')
+    res.send('jenkins depoly on staging')
 })
 
 app.listen(3333, ()=>console.log('server online na porta 3333'))
